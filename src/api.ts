9cde517f548e70b4d596a3fd8fe2011548530ffd
SaGaR KuMaR <sagarkumar863081@gmail.com>
2023-03-15 10:32:51 +0530

    welcome

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
 import { movies } from "./Models/movietype";
 
-const API = "?api_key=3ac20e37c3b1bdd32dadec03d228864f";
+const APIKEY = "?api_key=3ac20e37c3b1bdd32dadec03d228864f";
 
-export const getproductslist = (id: number): Promise<movies[]> => {
-  return axios
-    .get("https://api.themoviedb.org/3/movie/" + id + API)
+export const moviedetailsapi = (id: number) => {
+  console.log
+  axios
+    .get("https://api.themoviedb.org/3/movie/ " + id + APIKEY)
     .then((res) => res.data);
 };
 
 export const searchapi = async (keyword: string) => {
-  console.log(keyword)
+  // console.log(keyword);
   const res = await axios.get(
     " https://api.themoviedb.org/3/search/movie?api_key=3ac20e37c3b1bdd32dadec03d228864f&query=" +
       keyword
